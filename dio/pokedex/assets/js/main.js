1ed2e16b4f2b928f101ab3b747cd317abf9431fd
AdilsonFrizon <adilsonfrizon@gmail.com>
2022-11-19 16:42:43 -0400

    finishing the pokedex

--- dio/pokedex/assets/js/main.js
+++ dio/pokedex/assets/js/main.js
@@ -1,8 +1,41 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-fetch(url) // fetch: processamento assíncrono - em algum momento se obterá a resposta, não na hora
-    .then((response) => response.json()) // () => outra maneira de chamar função
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+const maxRecords = 905
+const limit = 48
+let offset = 0
+
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+            const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+            `).join('')
+        
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecords = offset + limit
+    if(qtdRecords >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItems(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItems(offset, limit)
+    }
+})
