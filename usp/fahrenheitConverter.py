082a83251fb032db766fc9197e06cb6d46b35e79
AdilsonFrizon <adilsonfrizon@gmail.com>
2023-08-12 21:42:24 -0400

    course's exercises

---
+++ usp/fahrenheitConverter.py
@@ -0,0 +1,5 @@
+tempFahrenheit = float(input('Digite uma temperatura em Fahrenheit: '))
+
+tempCelsius = ((tempFahrenheit - 32) * 5 ) / 9
+
+print('A temperatura em Celsius é', tempCelsius)
---
+++ usp/var.py
@@ -0,0 +1,6 @@
+a = 5
+b = 9
+
+soma = a + b
+
+print("a soma dos números é", soma)
