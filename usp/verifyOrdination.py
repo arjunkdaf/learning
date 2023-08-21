5cadafc848136a6378608cd5250d17b239b6acbf
AdilsonFrizon <adilsonfrizon@gmail.com>
2023-08-21 19:55:03 -0400

    course's exercises

---
+++ usp/verifyOrdination.py
@@ -0,0 +1,9 @@
+numeroUm = int(input('Digite o primeiro número: '))
+numeroDois = int(input('Digite o segundo número: '))
+numeroTres = int(input('Digite o terceiro número: '))
+
+if numeroUm < numeroDois and numeroDois < numeroTres:
+    print('Os números estão em ordem crescente.')
+else:
+    print('Os números não estão em ordem crescente.')
+
