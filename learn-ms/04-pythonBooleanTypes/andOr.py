7332fd3e7bef3764f6fa2c8edf13b459c466d45c
AdilsonFrizon <adilsonfrizon@gmail.com>
2023-08-30 16:39:40 -0400

    first module

---
+++ learn-ms/04-pythonBooleanTypes/andOr.py
@@ -0,0 +1,7 @@
+objectSize = 10
+proximity = 9000
+
+if objectSize > 5 and proximity < 1000:
+    print("Manobra evasiva requerida.")
+else:
+    print("O objeto não oferece ameaça.")
---
+++ learn-ms/04-pythonBooleanTypes/if.py
@@ -0,0 +1,5 @@
+objectSize = 10
+if objectSize > 5:
+    print("Precisamos ficar atentos a esse objeto.")
+else:
+    print("O objeto não oferece ameaça.")
