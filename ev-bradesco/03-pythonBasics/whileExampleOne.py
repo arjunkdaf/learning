570b1afd44c70d33d67edd91f029dd67bf665a80
AdilsonFrizon <adilsonfrizon@gmail.com>
2023-09-01 21:08:19 -0400

    python basics

---
+++ ev-bradesco/03-pythonBasics/whileExampleOne.py
@@ -0,0 +1,13 @@
+# para imprimir de 1-13 com while
+num = 1 # var deve ser definida antes
+while num <= 13:
+    print(num)
+    num += 1 # var deve ser incrementada dentro do bloco
+
+print("//")
+
+# para imprimir de 13-1 com while
+num = 13
+while num >= 1:
+    print(num)
+    num -= 1
